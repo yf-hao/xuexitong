27defa9e2cc91ea8df99a46b96b3308aab509fe1
yfhao <hyfdbd@gmail.com>
2026-04-11 20:59:45 +0800

    chore: bump version to V0.5.1 and update release notes

--- core/config.py
+++ core/config.py
@@ -2,7 +2,7 @@ import os
 import sys
 
 # Constants and default configurations for the application
-APP_TITLE = "学习通教学资源管理系统V0.5.0"
+APP_TITLE = "学习通教学资源管理系统V0.5.1"
 
 # Default Institution ID (郑州西亚斯学院)
 DEFAULT_FID = "4311"
